d5ea4ec7eea7788ac03c480ad6c81c0f1bba256b
Austin Landis <austinlandiscode@gmail.com>
2021-01-15 09:16:01 -0800

    Add const variables = to the select# from the six questions.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $("form#language-suggest").submit(function(event) {
     event.preventDefault();
-    
+    const system = $("select#system").val();
+    const application = $("select#application").val();
+    const game = $("select#game").val();
+    const info = $("select#info").val();
+    const garbage = $("select#garbage").val();
+    const color = $("select#color").val();
   })
 })
