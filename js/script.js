d1d64ba51359f4ab4cf963386d277eea2f257a9b
Austin Landis <austinlandiscode@gmail.com>
2021-01-15 14:21:24 -0800

    Add a spans with classes to reveal the entered name in our hidden divs.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form#language-suggest").submit(function(event) {
     event.preventDefault();
-    const name = $("input#user-name").val
+    const name = $("input#user-name").val();
     const system = $("select#system").val();
     const application = $("select#application").val();
     const game = $("select#game").val();
