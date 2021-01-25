fa90ba1884988322fb4763e0c29d55a349003030
Austin Landis <austinlandiscode@gmail.com>
2021-01-24 21:02:02 -0800

    fixed formatting, added more branching.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-$(document).ready(function() {
-  $("form#language-suggest").submit(function(event) {
+$(document).ready(function () {
+  $("form#language-suggest").submit(function (event) {
     event.preventDefault();
+    $("#swift").hide();
+    $("#ruby").hide();
+    $("#javascript").hide();
+    $("#csharp").hide();
+    $("#go").hide();
+    $("#rust").hide();
+    $("#python").hide();
+
     const name = $("input#user-name").val();
     const system = $("select#system").val();
     const application = $("select#application").val();
@@ -8,25 +16,45 @@ $(document).ready(function() {
     const info = $("select#info").val();
     const garbage = $("select#garbage").val();
     const color = $("select#color").val();
-
+    console.log(system);
     $(".name").text(name);
-
-    if (system === "MacOs" && application === "Mobile") {
-      $('#swift').show();
+      if (!name) {
+      alert("Please enter a name.");
+    } else if (
+      system === "- select answer below -" ||
+      application === "- select answer below -" ||
+      game === "- select answer below -" ||
+      info === "- select answer below -" ||
+      garbage === "- select answer below -" ||
+      color === "- select answer below -") {
+      alert("Please complete the form!");
+    } else if (system === "MacOs" && application === "Mobile") {
+      $("#swift").show();
     } else if (system === "MacOs" && garbage === "Yes" && color === "Red") {
-      $('#ruby').show();
+      $("#ruby").show();
     } else if (system === "MacOS" && garbage === "Yes" && color === "Yellow") {
-      $('#javascript').show();
-    } else if (system === "Windows" && application === "Web" && info === "Not Sensitive" || info === "Sensitive") {
-      $('#csharp').show();
-    } else if (info === "Sensitive" || info === "Very Sensitive" && garbage === "Yes" && application === "Web") {
-      $('#go').show();
-    } else if (info === "Very Sensitive" && game === "No" && system === "MacOS" || system === "Windows") {
-      $('#rust').show();
+      $("#javascript").show();
+    } else if (
+      (system === "Windows" &&
+        application === "Web" &&
+        info === "Not Sensitive") ||
+      info === "Sensitive"
+    ) {
+      $("#csharp").show();
+    } else if (
+      info === "Sensitive" ||
+      (info === "Very Sensitive" && garbage === "Yes" && application === "Web")
+    ) {
+      $("#go").show();
+    } else if (
+      (info === "Very Sensitive" && game === "No" && system === "MacOS") ||
+      system === "Windows"
+    ) {
+      $("#rust").show();
     } else if (game === "Yes") {
-      $('#python').show();
+      $("#python").show();
     } else {
-      $('#javascript').show();
+      $("#javascript").show();
     }
   });
 });
